d8194c9cce6057308aa8ddcfeec9a5eb886ecac3
Margaret Amanfu <margaretfafa@gmail.com>
2023-06-06 15:18:28 -0700

    self assessment tasks

---
+++ task8.go
@@ -0,0 +1,28 @@
+// else if statement
+
+package main
+import ("fmt")
+
+func main () {
+	time := 20
+	if time < 10 {
+		fmt.Println("Good morming")
+	} else if time < 20 {
+		fmt.Println("Good day")
+	} else {
+		fmt.Println("Good evening")
+	}
+
+// another example of the else if statement
+
+a := 14
+b := 14
+
+if a < b {
+	fmt.Println("a is less than b")
+} else if a > b {
+	fmt.Println("a is greater than b")
+} else {
+	fmt.Println("a is equal to b")
+}
+ }
