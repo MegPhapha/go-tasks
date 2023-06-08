fcae2881f40b97f8e35ff2fd014c91aff168cc6b
Margaret Amanfu <margaretfafa@gmail.com>
2023-06-08 16:58:21 -0700

    self assessment task

--- task8.go
+++ task8.go
@@ -25,4 +25,19 @@ if a < b {
 } else {
 	fmt.Println("a is equal to b")
 }
- }
+ 
+
+ // nested if statements
+
+ 
+	num := 20
+	if num >= 10 {
+		fmt.Println("Num is more than 10.")
+		if num > 15 {
+			fmt.Println("Num is also more than 15.")
+			 }
+		} else {
+			fmt.Println("Num is less than 10.")
+		}
+	}  
+ 
